b5fb9c8996df0428bc2154b1996f6ae621efc962
AnishBacchus <anishbacchus@gmail.com>
2026-07-21 17:02:52 -0400

    Created track and tire dictionaries to store data based on track and tire compound. Created a function to calculate degradation track and tire. Updated lap_time function.

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-def lap_time(base_time, degradation_rate, tire_age):
-    return base_time + (degradation_rate * tire_age)
+
+#Dictionarires for tracks and tire compounds
+
+TRACKS = {
+    "Spa": {
+        "base_time": 100,
+        "pit_stop_cost": 18.5,
+        "degradation_rate": 1.20
+    },
+
+    "Melbourne": {
+        "base_time": 80,
+        "pit_stop_cost": 23,
+        "degradation_rate": 0.90
+    },
+
+     "Suzuka": {
+        "base_time": 84,
+        "pit_stop_cost": 23.75,
+        "degradation_rate": 1.30
+    }
+}
+
+TIRES = {
+    "soft": 1.5,
+    "medium": 1.0,
+    "hard": 0.7
+}
+
+def lap_time(track, tire_compound):
+    return TRACKS[track]["base_time"] + actual_degradation_rate(track, tire_compound)
 
 
-print(lap_time(90, 0.3, 5))
+def actual_degradation_rate(track, tire_compound):
+    return round(TRACKS[track]["degradation_rate"] * TIRES[tire_compound], 2)
+
